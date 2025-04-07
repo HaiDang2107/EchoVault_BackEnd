1ec33958eaa5a32ce854131458506ff7068d258b
HaiDang <haidangvu2107@gmail.com>
2025-04-08 02:37:19 +0700

    logout

--- src/module/auth/auth.controller.ts
+++ src/module/auth/auth.controller.ts
@@ -1,8 +1,8 @@
-import { Controller, Post, Get, Body, Request, UseGuards } from '@nestjs/common';
+import { Controller, Post, Get, Body, Request, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
 import { LocalAuthGuard } from './guard/local-auth.guard';
-import { JwtAuthGuard } from './guard/jwt-auth.guard';
+import { Response } from 'express';
 import { GoogleAuthGuard } from './guard/google-auth.guard';
 
 @Controller('auth')
@@ -28,7 +28,7 @@ export class AuthController {
   }
 
   @Post('signup')
-  signup(@Body() dto: AuthDto) {
+  async signup(@Body() dto: AuthDto) {
     // extract data from the body of a HTTP request
     console.log({
       dto,
@@ -38,8 +38,27 @@ export class AuthController {
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
-  login(@Request() req) {
-    const token = this.authService.login(req.user);
-    return token;
+  async login(@Request() req, @Res() res: Response) {
+    const token = await this.authService.login(req.user);
+
+    // Lưu JWT vào cookie
+    res.cookie('jwt', token.access_token, {
+      httpOnly: true,  
+      sameSite: 'strict', // Ngăn gửi cookie trong yêu cầu cross-site
+      maxAge: 3600000,  // Thời gian sống của cookie (1 giờ)
+    });
+
+    return res.status(200).json({ message: 'Log in successfully' });;
+  }
+
+  @Post('logout')
+  async logout(@Res() res: Response) {
+
+    res.clearCookie('jwt', { 
+      httpOnly: true,  
+      sameSite: 'strict',
+    });
+    
+    return res.status(200).json({ message: 'Log out successfully' });
   }
 }
