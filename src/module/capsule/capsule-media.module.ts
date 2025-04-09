167e7616304ae90b6597a5e4e13ed3ffe894d026
vietnamdzjj <vietnamdzjj@gmail.com>
2025-04-10 00:18:39 +0700

    Update capsule-media.module.ts

--- src/module/capsule/capsule-media.module.ts
+++ src/module/capsule/capsule-media.module.ts
@@ -2,10 +2,10 @@
 import { Module } from '@nestjs/common';
 import { CapsuleMediaService } from './capsule-media.service';
 import { CapsuleMediaController } from './capsule-media.controller';
-import { PrismaService } from './prisma.service';
 
 @Module({
   providers: [CapsuleMediaService, PrismaService],
   controllers: [CapsuleMediaController],
+  exports: [CapsuleMediaService],
 })
 export class CapsuleMediaModule {}
